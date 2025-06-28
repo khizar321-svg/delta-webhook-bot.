a3757dff82f2603f7fa63163be81e46f08ef6a3c
khizar321-svg <khizarqureshi334@gmail.com>
2025-06-28 20:02:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,6 @@ def webhook():
     return {"code": 200, "message": "Order placed"}
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
   
